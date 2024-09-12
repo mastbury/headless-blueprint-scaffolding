a2d7b2acabe14e2f66ac05f37a76c176b1855fb7
OfficialCommunityWP <mike.astbury@officialcommunity.com>
2024-09-12 14:50:21 -0400

    adding footer menu from WP

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,42 @@
 import styles from "./footer.module.css";
+import { gql, useQuery } from "@apollo/client";
+
+const FOOTER_MENU_QUERY = gql`
+query FooterMenu {
+  menuItems(where: {location: FOOTER}) {
+    nodes {
+      id
+      uri
+      menuItemId
+      label
+    }
+  }
+}
+`;
 
 export default function Footer() {
+
+  // Use the StickyNews query
+  const { data, loading, error } = useQuery(FOOTER_MENU_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    console.error("Error fetching footer menu!:", error);
+    return <p>Error: {error.message}</p>;
+  }
+
+  const menuItems = data.menuItems.nodes;
+
+
+  {console.log(menuItems);}
   return (
     <footer className={styles.footer}>
       &copy; JimCuddy.com {new Date().getFullYear()}. All Rights Reserved.
+      <ul className="footer-menu">
+        {menuItems.map((item) => (
+          <li key={item.id}>{item.label}</li>
+        ))}
+      </ul>
     </footer>
   );
 }
